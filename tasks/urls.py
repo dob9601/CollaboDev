de94934b4b1d29737c2e265687214a2741774b8e
dob9601 <dob9601@gmail.com>
2018-01-17 17:48:40 +0000

    Removal of obsolete 'create task' page from urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,6 @@ from . import task_actions
 app_name = 'tasks'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create/', task_actions.create, name='create'),
     path('submit/', task_actions.submit, name='submit'),
     path('delete/', views.delete, name='delete'),
     path('claim/', task_actions.claim, name='claim'),
