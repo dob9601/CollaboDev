ed9f65578cda0a985d6dc349279d9732cbc709a1
dob9601 <dob9601@gmail.com>
2018-01-18 16:27:21 +0000

    Modal bug fixes related to recent change along with improved modal formatting

--- tasks/static/tasks/js/modal.js
+++ tasks/static/tasks/js/modal.js
@@ -1,6 +1,7 @@
 function show_modal(data_type, data) {
 	const modal_box = document.getElementsByClassName('modal-box')[0];
 	const modal_text = modal_box.children[0].children[1];
+
 	if (data_type == 'task_data') {
 		if (data[6] == 'True') {
 			task_status = "Open";
@@ -16,12 +17,11 @@ function show_modal(data_type, data) {
 				 	  '<b>Age: </b>'+document.getElementById(data[0]).children[4].innerHTML+'<br>' +
 	  				  '<b>Deadline: </b>'+data[5]+'<br>' +
 				  	  '<b>Status: </b> '+task_status+'<br>'
-		modal_box.style.display = 'block';
 	}
 	else if(data_type == 'create_task') {
 		modal_text.innerHTML = '<h3>Create Task</h3>' +		
 					  '<form action="/tasks/submit/" method="post">' +
-					  "{% csrf_token %}" +
+					  '<input type="hidden" name="csrfmiddlewaretoken" value="'+data+'">' +
 					  '<label>Task Name: </label><input type="text" name="task_name" id="task_name" autocomplete="off"><br>' +
 					  '<label>Task Description: </label><input type="text" name="task_description" id="task_description" autocomplete="off"><br>' +
 					  '<label>Task Owner: </label><input type="text" name="task_owner" id="task_owner" autocomplete="off"><br>' +
@@ -49,8 +49,8 @@ function show_modal(data_type, data) {
 					  '<label>Deadline: </label><input type="date" name="deadline_date" id="deadline_date" autocomplete="off"><br>' +
 					  '<input id="create-task-button" type="submit" value="Create" />' +
 			  		  '</form>'
-		modal_box.style.display = 'block';
 	}
+	modal_box.style.display = 'block';
 }
 
 var modal = document.getElementsByClassName('modal-box')[0];
