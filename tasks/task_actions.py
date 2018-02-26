d18087e87201a8076953374e3ca9bf3350b72356
dob9601 <dob9601@gmail.com>
2018-02-26 16:05:19 +0000

    Major adjustments to task claiming, closing and unclaiming

--- tasks/task_actions.py
+++ tasks/task_actions.py
@@ -22,11 +22,16 @@ def submit(request):
     else:
         deadline_date = datetime(1, 1, 1)
 
+    if request.POST['task_owner'] != '':
+        task_owner = User.objects.get(username=request.POST['task_owner'])
+    else:
+        task_owner = None
+
     try:
         new_task = Task(
             task_name=request.POST['task_name'],
             task_description=request.POST['task_description'],
-            task_owner=request.POST['task_owner'],
+            task_owner=task_owner,
             task_priority=request.POST['task_priority'],
             deadline_date=deadline_date,
             task_size=request.POST['task_size'],
@@ -36,8 +41,7 @@ def submit(request):
 
         request.session['response_message'] = 4
         return HttpResponseRedirect(reverse('tasks:index'))
-    except ValidationError as error:
-        print(error)
+    except ValidationError:
         request.session['response_message'] = 5
         return HttpResponseRedirect(reverse('tasks:index'))
 
@@ -49,21 +53,25 @@ def claim(request):
     """
     try:
         task_id = request.POST['task_id']
-        new_owner = request.user.username
-        chosen_task = Task.objects.get(pk=int(task_id))
-        old_owner = chosen_task.task_owner
+        new_owner = request.user
+        task = Task.objects.get(pk=int(task_id))
+        old_owner = task.task_owner
 
-        if old_owner == "" and request.user.profile.current_task is None:
-            chosen_task.task_owner = new_owner
-            chosen_task.save()
+        if old_owner is None:
+            try:
+                previous_task = request.user.profile.current_task
+                previous_task.task_owner = None
+                previous_task.save()
+            except AttributeError:
+                pass
+
+            task.task_owner = new_owner
+            task.save()
             request.session['response_message'] = 1
 
-            request.user.profile.current_task = chosen_task
+            request.user.profile.current_task = task
             request.user.save()
 
-            return HttpResponseRedirect(reverse('tasks:index'))
-        elif request.user.profile.current_task is None:
-            request.session['response_message'] = 8
             return HttpResponseRedirect(reverse('tasks:index'))
 
         request.session['response_message'] = 2
@@ -81,12 +89,13 @@ def unclaim(request):
     """
     try:
         task_id = request.POST['task_id']
-        chosen_task = Task.objects.get(pk=int(task_id))
-        chosen_task.task_owner = ""
-        chosen_task.save()
+        task = Task.objects.get(pk=int(task_id))
+        task.task_owner = None
+        task.save()
 
-        request.user.profile.current_task = None
-        request.user.save()
+        user = request.user
+        user.profile.current_task = None
+        user.save()
 
         request.session['response_message'] = 6
         return HttpResponseRedirect(reverse('tasks:index'))
@@ -107,8 +116,8 @@ def close(request):
         chosen_task.task_open = False
         chosen_task.save()
 
-        current_user = request.user.id
-        user = User.objects.get(id=current_user)
+        user = request.user
+        user.profile.current_task = None
         user.profile.tasks_completed += chosen_task.task_size
         user.save()
 
