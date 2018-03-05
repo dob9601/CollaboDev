9de32cc1332dfe9269d1a8e64855b644532a7dcc
dob9601 <dob9601@gmail.com>
2018-03-05 18:14:24 +0000

    Migration of profile model to accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from tasks.models import Task
+
+
+class Profile(models.Model):
+    """
+    Extension of user model, added automatically upon user creation.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_task = models.OneToOneField(
+        Task,
+        on_delete=models.CASCADE,
+        null=True,
+        default=None,
+    )
+
+    associated_image = models.CharField(
+        default='/accounts/images/default_avatar.png',
+        max_length=1000
+    )
+    gravatar_url = models.CharField(
+        default='',
+        max_length=1000
+    )
+    gravatar_enabled = models.BooleanField(default=False)
+
+    url = models.CharField(max_length=200, blank=True)
+    biography = models.CharField(max_length=300, blank=True)
+    tasks_completed = models.IntegerField(default=0)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create profile model upon user creation
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- tasks/models.py
+++ tasks/models.py
@@ -4,8 +4,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 from django.utils.timezone import now
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Task(models.Model):
@@ -26,7 +24,7 @@ class Task(models.Model):
     task_name = models.CharField(max_length=70, blank=False)
     task_description = models.CharField(max_length=400)
     task_owner = models.ForeignKey(User, on_delete=models.CASCADE,
-                                      blank=True, default=None, null=True)
+                                   blank=True, default=None, null=True)
     task_priority = models.IntegerField(validators=[MaxValueValidator(10),
                                                     MinValueValidator(1)],
                                         blank=False)
@@ -41,41 +39,3 @@ class Task(models.Model):
 
     def __str__(self):
         return self.task_name
-
-
-class Profile(models.Model):
-    """
-    Extension of user model, added automatically upon user creation.
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    tasks_completed = models.IntegerField(default=0)
-    current_task = models.OneToOneField(
-        Task,
-        on_delete=models.CASCADE,
-        null=True,
-        default=None,
-    )
-    associated_image = models.CharField(
-        default='/accounts/images/default_avatar.png',
-        max_length=1000
-    )
-    gravatar_url = models.CharField(
-        default='',
-        max_length=1000
-    )
-    gravatar_enabled = models.BooleanField(default=False)
-    quest = models.CharField(default='null', max_length=1000)
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """
-    Create profile model upon user creation
-    """
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
