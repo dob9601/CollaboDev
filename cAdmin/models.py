e827740fcc31fa0642d90b5bb58b7241fb77e373
dob9601 <dob9601@gmail.com>
2018-03-05 17:44:12 +0000

    Addition of settings model

--- cAdmin/models.py
+++ cAdmin/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Settings(models.Model):
+    # GitHub settings
+    github_integrated = models.BooleanField(default=False)
+
+    # Profile settings
+    profile_allow_biography = models.BooleanField(default=True)
+    profile_allow_name_changes = models.BooleanField(default=False)
+    profile_allow_username_changes = models.BooleanField(default=False)
+
+    profile_allow_change_requests = models.BooleanField(default=True)
+
+    # User settings
+    users_show_completed_tasks = models.BooleanField(default=True)
+    users_show_email = models.BooleanField(default=True)
+    users_show_online = models.BooleanField(default=True)
+
+    # Tasks settings
+    tasks_show_xp_bar = models.BooleanField(default=True)
