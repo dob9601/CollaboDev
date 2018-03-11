29d546729479edb8fe15db1f049da911f911297c
dob9601 <dob9601@gmail.com>
2018-03-11 09:05:45 +0000

    js moved out of html file and css restructured to be in a css folder

---
+++ cAdmin/static/admin/first-time-setup-2/js/updateDivs.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+function updateDivs (relativeDistance) {
+	const form = document.getElementById('main-form')
+	var currentPosition
+
+	for (var i = 1; i < form.children.length; i++) {
+		if (form.children[i].style.display !== 'none') {
+			if ((i + relativeDistance < 1) || (i + relativeDistance > form.children.length - 1)) {
+				return false
+			}
+
+			const nextButton = document.getElementsByClassName('buttons')[0].children[1]
+			if (i + relativeDistance === form.children.length - 1) {
+				nextButton.innerHTML = 'Submit'
+				nextButton.onclick = function () {
+					document.getElementById('main-form').submit()
+				}
+			} else {
+				nextButton.innerHTML = 'Next'
+				nextButton.onclick = function () {
+					updateDivs(1)
+				}
+			}
+			currentPosition = i
+		}
+		form.children[i].style.display = 'none'
+	}
+	form.children[currentPosition + relativeDistance].style.display = 'block'
+}
