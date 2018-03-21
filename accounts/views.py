ded3caa69168457b8efb73b70008f5f0511099ee
dob9601 <dob9601@gmail.com>
2018-03-21 18:00:35 +0000

    Settings form now works for all profile settings

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -28,6 +30,8 @@ def settings(request):
     context = {
         'error_first_name': False,
         'error_last_name': False,
+        'error_biography': False,
+        'error_url': False,
 
         'error_old_pword': False,
         'error_pword_conf': False,
@@ -43,25 +47,47 @@ def settings(request):
         user_profile_biography = request.POST['profile_biography']
         user_profile_url = request.POST['profile_url']
 
+        # Profile
         if user_first_name == '':
             context['error_first_name'] = True
+        else:
+            user.first_name = user_first_name
+            context['successful_changes'].append('first name')
+
         if user_last_name == '':
             context['error_last_name'] = True
+        else:
+            user.last_name = user_last_name
+            context['successful_changes'].append('last name')
+        
+        if len(user_profile_biography) > 300:
+            context['error_biography'] = True
+        else:
+            user.profile.biography = user_profile_biography
+            context['successful_changes'].append('biography')
 
+        if '://' not in user_profile_url:
+            user_profile_url = 'http://' + user_profile_url
+        try:
+            url_validate = URLValidator()
+            url_validate(user_profile_url)
+
+            user.profile.url = user_profile_url
+            context['successful_changes'].append('last name')
+        except ValidationError:
+            context['error_url'] = True
+        # End of profile
+
+
+        # Account
         user_username = request.POST['username']
+        # End of account
+
 
         # Password
         user_old_pword = request.POST['old_pword']
         user_new_pword = request.POST['new_pword']
         user_new_pword_conf = request.POST['new_pword_conf']
-        
-        if not context['error_first_name']:
-            user.first_name = user_first_name
-            context['successful_changes'].append('first name')
-        if not context['error_last_name']:
-            user.last_name = user_last_name
-            context['successful_changes'].append('last name')
-
 
         if user_old_pword != '' or user_new_pword != '':
             # Old password validation
@@ -69,9 +95,9 @@ def settings(request):
                 context['error_old_pword'] = True
     
             # New password validation
-            if user_new_pword != user_new_pword_conf:
+            elif user_new_pword != user_new_pword_conf:
                 context['error_pword_conf'] = True
-            if not context['error_pword_conf'] and not context['error_old_pword']:
+            else:
                 user.set_password(user_new_pword)
                 update_session_auth_hash(request, user)
                 context['successful_changes'].append('password')
