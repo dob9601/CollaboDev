35adee98db0d7e50704137cc4c5be04e3822818e
dob9601 <dob9601@gmail.com>
2018-05-24 17:45:59 +0100

    Patch to import

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-import user_verification
+from . import user_verification
 
 
 @login_required
