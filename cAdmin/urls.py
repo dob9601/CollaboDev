917187b2c10db124fb3e3af140e1194dfdf8a4aa
dob9601 <dob9601@gmail.com>
2018-06-03 11:53:42 +0100

    Removal of obsolete admin page

--- cAdmin/urls.py
+++ cAdmin/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'cAdmin'
 urlpatterns = [
-    path('', views.index, name='index'),
     path('users/', views.users, name='users'),
     path('users/delete/', views.delete_user, name='delete_user'),
     path('users/create/', views.create_user, name='create_user'),
