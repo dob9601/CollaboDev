79592b0e12d377c878f175cee796262abd85fda7
dob9601 <dob9601@gmail.com>
2018-09-26 23:04:51 +0100

    Linting patch

--- tasks/static/tasks/js/create_buttons.js
+++ tasks/static/tasks/js/create_buttons.js
@@ -5,44 +5,43 @@ var currentUser = document.currentScript.getAttribute('current-user')
 var userAdmin = document.currentScript.getAttribute('user-admin')
 
 if (taskOwner === 'None') {
-    target.value = 'Claim'
-    target.style.backgroundColor = '#2ECC40'
-    target.style.cursor = 'pointer'
-    if (userAdmin) {
-        target.style.borderRadius = '8px 8px 0 0'
-    }
+  target.value = 'Claim'
+  target.style.backgroundColor = '#2ECC40'
+  target.style.cursor = 'pointer'
+  if (userAdmin) {
+    target.style.borderRadius = '8px 8px 0 0'
+  }
 } else {
-    target.style.backgroundColor = '#FF851B'
-    target.style.cursor = 'not-allowed'
-    target.disabled = true
-    if (taskOwner === currentUser) {
-        target.value = 'Claimed by you'
+  target.style.backgroundColor = '#FF851B'
+  target.style.cursor = 'not-allowed'
+  target.disabled = true
+  if (taskOwner === currentUser) {
+    target.value = 'Claimed by you'
 
-        const unclaimElements = document.getElementsByClassName('unclaim')
-        const unclaim = unclaimElements[unclaimElements.length - 1]
-        const closeElements = document.getElementsByClassName('close-task')
-        const close = closeElements[closeElements.length - 1]
-        unclaim.parentNode.style = 'display: inline'
-        unclaim.style.backgroundColor = '#AAAAAA'
-        unclaim.style.cursor = 'pointer'
+    const unclaimElements = document.getElementsByClassName('unclaim')
+    const unclaim = unclaimElements[unclaimElements.length - 1]
+    const closeElements = document.getElementsByClassName('close-task')
+    const close = closeElements[closeElements.length - 1]
+    unclaim.parentNode.style = 'display: inline'
+    unclaim.style.backgroundColor = '#AAAAAA'
+    unclaim.style.cursor = 'pointer'
 
-        close.parentNode.style = 'display: inline'
-        close.style.backgroundColor = '#2ECC40'
-        close.style.cursor = 'pointer'
+    close.parentNode.style = 'display: inline'
+    close.style.backgroundColor = '#2ECC40'
+    close.style.cursor = 'pointer'
 
-        target.style.borderRadius = '8px 8px 0 0'
-        close.style.borderRadius = '0'
+    target.style.borderRadius = '8px 8px 0 0'
+    close.style.borderRadius = '0'
 
-        if (!userAdmin) {
-            unclaim.style.borderRadius = '0 0 8px 8px'
-        }
-        else {
-            unclaim.style.borderRadius = '0'
-        }
+    if (!userAdmin) {
+      unclaim.style.borderRadius = '0 0 8px 8px'
     } else {
-        target.value = 'Claimed by ' + taskOwner
-        if (userAdmin) {
-            target.style.borderRadius = '8px 8px 0 0'
-        }
+      unclaim.style.borderRadius = '0'
     }
+  } else {
+    target.value = 'Claimed by ' + taskOwner
+    if (userAdmin) {
+      target.style.borderRadius = '8px 8px 0 0'
+    }
+  }
 }
